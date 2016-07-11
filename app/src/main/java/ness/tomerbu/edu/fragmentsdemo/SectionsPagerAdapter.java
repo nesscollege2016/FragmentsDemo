07756078a1da839ff35069f2831f5375ba357e56
nesscollege2016 <nesscollege2016@gmail.com>
2016-07-11 10:57:06 +0300

    tabs activity

---
+++ app/src/main/java/ness/tomerbu/edu/fragmentsdemo/SectionsPagerAdapter.java
@@ -0,0 +1,51 @@
+package ness.tomerbu.edu.fragmentsdemo;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+
+            switch (position) {
+                case 0:
+                    return new DateFragment();
+                case 1:
+                    return new ColorFragment();
+                case 2:
+                    return new TimePickerFragment();
+            }
+            return null;
+            
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return "Date";
+                case 1:
+                    return "Color";
+                case 2:
+                    return "Time";
+            }
+            return null;
+        }
+    }
